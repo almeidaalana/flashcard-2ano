8411d4ea35a5196ce502621be37f34bf44a4bc84
almeidaalana <141160023+almeidaalana@users.noreply.github.com>
2024-10-24 11:14:46 -0300

    okk

--- perguntas.js
+++ perguntas.js
@@ -3,3 +3,28 @@ criaCartao(
     'O que é HTML?',
     'É uma linguagem de marcação de Hipertexto',
 )
+criaCartao(
+    'Programção',
+    'O que é HTML?',
+    'É uma linguagem de marcação de Hipertexto',
+)
+criaCartao(
+    'Programção',
+    'O que é HTML?',
+    'É uma linguagem de marcação de Hipertexto',
+)
+criaCartao(
+    'Programção',
+    'O que é HTML?',
+    'É uma linguagem de marcação de Hipertexto',
+)
+criaCartao(
+    'Programção',
+    'O que é HTML?',
+    'É uma linguagem de marcação de Hipertexto',
+)
+criaCartao(
+    'Programção',
+    'O que é HTML?',
+    'É uma linguagem de marcação de Hipertexto',
+)
