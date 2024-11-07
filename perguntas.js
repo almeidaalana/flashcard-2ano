8d0b27711db9449623e2f6f6c1a2421705f1c6e3
almeidaalana <141160023+almeidaalana@users.noreply.github.com>
2024-11-07 11:53:18 -0300

    ok

--- perguntas.js
+++ perguntas.js
@@ -1,30 +1,30 @@
 criaCartao(
-    'Programção',
-    'O que é HTML?',
-    'É uma linguagem de marcação de Hipertexto',
+    'História',
+    'O que foi a era vargas?',
+    'Peíodo da históriado Brasil emtre 1930 e 1945, qundo Getúlio Vargas governou o Brasil por 15 anos',
 )
 criaCartao(
-    'Programção',
-    'O que é HTML?',
-    'É uma linguagem de marcação de Hipertexto',
+    'Geografia',
+    'O que é relevo?',
+    'É o conjunto de formas que observamos na superfície terrestre',
 )
 criaCartao(
-    'Programção',
-    'O que é HTML?',
-    'É uma linguagem de marcação de Hipertexto',
+    'Biologia',
+    'O que é Genética?',
+    'É o ramo que estuda a hereditariedade que são transmitidas de uma geração para a outra',
 )
 criaCartao(
-    'Programção',
-    'O que é HTML?',
-    'É uma linguagem de marcação de Hipertexto',
+    'Sociologia',
+    'Quem é Max Werber?',
+   'Ele foi considerado um dos pais fundadores da sociologia',
 )
 criaCartao(
-    'Programção',
-    'O que é HTML?',
-    'É uma linguagem de marcação de Hipertexto',
+    'Artes',
+    'O que foi o mundo conteporâneo?',
+    'foi o peŕiodo de consolidação do capitalismo como o modo de produção e sua espanção por todo o mundo',
 )
 criaCartao(
-    'Programção',
-    'O que é HTML?',
-    'É uma linguagem de marcação de Hipertexto',
+    'Matemática',
+    'O que é função?',
+    'É a relação de um conjunto com um conjunto',
 )
