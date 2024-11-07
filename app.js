d2f4be37f628a902d63f31aebf1aa36247fd5346
almeidaalana <141160023+almeidaalana@users.noreply.github.com>
2024-11-07 12:02:05 -0300

    ok

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
-function criarCartao(categoria,pergunra,resposta){
-    console.log(categoria,pergunra,resposta);
+function criarCartao(categoria,pergunta,resposta){
+    let container = document.getElementyById('cotainer')
+    //VARIAVEL COTAINER BUSCADA PELO IDE 'cotainer' 
+    let cartao = document.createElement('article')
+    //VARIAVEL CARTAO QUE CRIA <ARTICLE> NO HTML
+    cartao.className = 'cartao'
+    //CRIA CLASSE "cartao" para o elemento criado
+    cartao.innerHTML = 
+    <div class="cartao__conteudo">
+        <h3>${categoria}</h3>
+        <div class="cartao__conteudo__pergunta">
+            <p></p>
+    </div>
 
-}
+    
     
